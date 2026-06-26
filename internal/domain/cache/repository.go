e39633df937931fb88b5e580f5279f0d83eb73d2
Labib <labibfaisal450@gmail.com>
2026-06-26 16:23:08 +0600

    feat: cache and rate limiter seperate

--- internal/domain/cache/repository.go
+++ internal/domain/cache/repository.go
@@ -9,3 +9,7 @@ type Cache interface {
 	Set(ctx context.Context, key string, value string, expire time.Duration) error
 	Get(ctx context.Context, key string) (string, error)
 }
+
+type RateLimiter interface {
+	RunScript(ctx context.Context, key string, capacity int, rate int, now int64) (interface{}, error)
+}
