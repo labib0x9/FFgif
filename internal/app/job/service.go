0f4a5243d4a1c9346bf3e68d256c222e0db2c171
Labib <labibfaisal450@gmail.com>
2026-06-28 20:43:03 +0600

    feat: process video in job service

--- internal/app/job/service.go
+++ internal/app/job/service.go
@@ -4,25 +4,34 @@ import (
 	"context"
 
 	"github.com/labib0x9/ffgif/internal/domain/cache"
+	"github.com/labib0x9/ffgif/internal/domain/media"
+	"github.com/labib0x9/ffgif/internal/domain/processor"
 	"github.com/labib0x9/ffgif/internal/domain/queue"
 )
 
 type Service interface {
 	Convert(ctx context.Context, userId string, key string, start float32, end float32, fps int, width int, loop bool) (*ConvertResult, error)
 	Status(ctx context.Context, jobId string) (*StatusResult, error)
+	Process(ctx context.Context, msg queue.VideoMessage) error
 }
 
 type service struct {
-	cache cache.Cache
-	queue queue.Queue
+	processor processor.VideoProcessor
+	gifRepo   media.GifRepository
+	cache     cache.Cache
+	queue     queue.Queue
 }
 
 func NewService(
+	processor processor.VideoProcessor,
+	gifRepo media.GifRepository,
 	cache cache.Cache,
 	queue queue.Queue,
 ) Service {
 	return &service{
-		cache: cache,
-		queue: queue,
+		processor: processor,
+		gifRepo:   gifRepo,
+		cache:     cache,
+		queue:     queue,
 	}
 }
