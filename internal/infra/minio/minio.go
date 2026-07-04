17d9fd79238b4728002cfdd1fef6904cc4e3bd5e
Labib <labibfaisal450@gmail.com>
2026-07-04 11:38:56 +0600

    feat: added cors to bucket

--- internal/infra/minio/minio.go
+++ internal/infra/minio/minio.go
@@ -2,6 +2,7 @@ package minio
 
 import (
 	"context"
+	"log/slog"
 	"time"
 
 	"github.com/labib0x9/ffgif/config"
@@ -77,6 +78,26 @@ func Setup(client *minio.Client, cnf *config.Minio) error {
 		return err
 	}
 
+	getNotification, err := client.GetBucketNotification(ctx, cnf.TempBucket)
+	if err == nil {
+		slog.Info("Notification config", "config", getNotification)
+	}
+
+	getLifecycle, err := client.GetBucketLifecycle(ctx, cnf.TempBucket)
+	if err == nil {
+		slog.Info("Lifecycle config", "config", getLifecycle)
+	}
+
+	getCors, err := client.GetBucketCors(ctx, cnf.TempBucket)
+	if err == nil {
+		slog.Info("CORS config", "config", getCors)
+	}
+
+	getCors, err = client.GetBucketCors(ctx, cnf.StorageBucket)
+	if err == nil {
+		slog.Info("CORS config", "config", getCors)
+	}
+
 	return nil
 }
 
@@ -99,8 +120,9 @@ func bindBucket(ctx context.Context, client *minio.Client, bucket string) error 
 
 	cfg := notification.NewConfig(arn)
 	cfg.AddEvents(
-		notification.ObjectCreatedPut,
-		notification.ObjectCreatedCompleteMultipartUpload,
+		// notification.ObjectCreatedPut,
+		// notification.ObjectCreatedCompleteMultipartUpload,
+		notification.ObjectCreatedAll,
 	)
 
 	var notificationCfg notification.Configuration
