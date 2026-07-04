5756a0b0fab013b6bf3a5717da28034cf16f2427
Labib <labibfaisal450@gmail.com>
2026-07-04 22:35:02 +0600

    feat: allowed lowercase header content-type

--- internal/transport/http/middleware/cors.go
+++ internal/transport/http/middleware/cors.go
@@ -12,12 +12,13 @@ func Cors(next http.Handler) http.Handler {
 			"http://127.0.0.1:5173":  true,
 			"http://localhost:5173/": true,
 			"http://127.0.0.1:5173/": true,
+			"http://127.0.0.1:8080":  true,
 		}
 		if allowedOrigins[origin] {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 		}
 		w.Header().Set("Vary", "Origin")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, content-type")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH")
 		// w.Header().Set("Content-Type", "application/json")
 		next.ServeHTTP(w, r)
