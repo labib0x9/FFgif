96f514230b1e1873ac4bd6597a3eb20670f55ce7
Labib <labibfaisal450@gmail.com>
2026-07-09 00:09:48 +0600

    feat: returns proper status code and error

--- internal/app/auth/verify.go
+++ internal/app/auth/verify.go
@@ -13,23 +13,14 @@ func (s *service) Verify(token string) error {
 	verifier, err := s.verifierRepo.GetByHash(hash)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
-			// http.Error(w, "token expired or invalid", http.StatusGone)
-			// slog.Warn("Verify: token expired", "error", err, "token_hash", hash)
 			return auth.ErrInvalidToken
 		}
-		// http.Error(w, "internal server error, try again later", http.StatusInternalServerError)
-		// slog.Error("Verify: token fetch error", "error", err, "token_hash", hash)
 		return auth.ErrTokenFetchFailed
 	}
 
 	if err := s.authRepo.SetVerified(verifier.UserId); err != nil {
-		// http.Error(w, "internal server error", http.StatusInternalServerError)
-		// slog.Error("Verify: user verification error", "error", err, "user_id", verifier.UserId, "token_hash", hash)
 		return auth.ErrSetUserVerifiedFailed
 	}
 
-	if err := s.verifierRepo.Delete(verifier.Id); err != nil {
-		// slog.Error("Verify: failed to delete verifier", "error", err, "id", verifier.Id)
-	}
-	return nil
+	return s.verifierRepo.Delete(verifier.Id)
 }
