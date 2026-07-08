11ec12123c923e165923db8f89d284e04edfada0
Labib <labibfaisal450@gmail.com>
2026-07-09 00:05:43 +0600

    feat: implemented reset password db query inside transaction

--- internal/transport/http/handlers/auth/reset_password.go
+++ internal/transport/http/handlers/auth/reset_password.go
@@ -2,9 +2,11 @@ package auth
 
 import (
 	"encoding/json"
+	"errors"
 	"log/slog"
 	"net/http"
 
+	"github.com/labib0x9/ffgif/internal/domain/auth"
 	"github.com/labib0x9/ffgif/pkg/jsonio"
 )
 
@@ -17,16 +19,15 @@ type reqReset struct {
 func (h *Handler) ResetPasswordGet(w http.ResponseWriter, r *http.Request) {
 	token := r.URL.Query().Get("token")
 	if token == "" {
-		slog.Warn("ResetPasswordGet: email not exists")
-		http.Error(w, "Bad request", http.StatusBadRequest)
+		slog.Warn("ResetPasswordGet: token not found")
+		jsonio.SendError(w, "Bad request", http.StatusBadRequest)
 		return
 	}
 
 	token, err := h.srv.ResetPasswordGet(token)
 	if err != nil {
-		switch err {
-
-		}
+		slog.Warn("srv.ResetPasswordGet() failed:", "error", err)
+		jsonio.SendError(w, "expired or invalid token", http.StatusGone)
 		return
 	}
 
@@ -40,21 +41,26 @@ func (h *Handler) ResetPasswordPost(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 
 	if err := decoder.Decode(&req); err != nil {
-		http.Error(w, "Bad request", http.StatusBadRequest)
+		jsonio.SendError(w, "Bad request", http.StatusBadRequest)
 		slog.Warn("ResetPasswordPost: bad json body", "error", err)
 		return
 	}
 
 	if err := h.validate.Struct(req); err != nil {
-		http.Error(w, "field required", http.StatusUnprocessableEntity)
+		jsonio.SendError(w, "field required", http.StatusUnprocessableEntity)
 		slog.Warn("ResetPasswordPost: struct validation failed", "error", err)
 		return
 	}
 
-	if err := h.srv.ResetPasswordPost(r.Context(), req.Token, req.Password, req.ConfirmPassword); err != nil {
-		switch err {
-
+	err := h.srv.ResetPasswordPost(r.Context(), req.Token, req.Password, req.ConfirmPassword)
+	if err != nil && !errors.Is(err, auth.ErrMessageQueueFailed) {
+		switch {
+		case errors.Is(err, auth.ErrReseterTokenFatchFailed):
+			jsonio.SendError(w, "invalid or expired token", http.StatusGone)
+		default:
+			jsonio.SendError(w, "internal server error", http.StatusInternalServerError)
 		}
+		slog.Warn("srv.ResetPasswordPost(): failed", "error", err)
 		return
 	}
 
